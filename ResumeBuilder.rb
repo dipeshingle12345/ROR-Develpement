732d1f559c845c1d6c8163116a206720397d6943
dipeshingle12345 <mfsi.dipeshi@gmail.com>
2023-03-03 11:06:50 +0530

    user input

--- ResumeBuilder.rb
+++ ResumeBuilder.rb
@@ -7,114 +7,119 @@ class PersonalDetails
         @place=args[:place]
         @preferred_location=args[:preferred_location]
     end
-    def getpersonaldetails
-        [@name,@email,@mobile,@place,@preferred_location]
-    end
+ 
 end
 
 class Experience
-    def initialize(company_name,current_post,joining_date,exiting_date)
-        @company_name=company_name
-        @current_post=current_post
-        @joining_date=joining_date
-        @exiting_date=exiting_date
+    attr_reader :company_name,:current_post,:joining_date,:exiting_date
+    def initialize(**args)
+        @company_name=args[:company_name]
+        @current_post=args[:current_post]
+        @joining_date=args[:joining_date]
+        @exiting_date=args[:exiting_date]
 
     end
-    def getexperiencedetails
-        [@company_name,@current_post,@joining_date,@exiting_date]
-    end
+   
 end
 
 
 class Skill
-    def initialize(name,level,duration)
-        @name=name
-        @level=level
-        @duration=duration
+    attr_reader :name,:level,:duration
+    def initialize(**args)
+        @name=args[:name]
+        @level=args[:level]
+        @duration=args[:duration]
 
 
     end
-    def getskilldetails
-        [@name,@level,@duration]
-    end
+   
 end
 
 
 
 class Qualifications
-    def initialize(course,college,board,start_date,end_date,marks)
-        @course=course
-        @college=college
-        @board=board
-        @start_date=start_date
-        @end_date=end_date
-        @marks=marks
+    attr_reader :course,:college,:board,:start_date,:end_date,:marks
+    def initialize(**args)
+        @course=args[:course]
+        @college=args[:college]
+        @board=args[:board]
+        @start_date=args[:start_date]
+        @end_date=args[:end_date]
+        @marks=args[:marks]
     end
-    def getqualificationsdetails
-        [@course,@college,@board,@start_date,@end_date,@marks]
-    end
+   
 end
 
 
 
 class Training 
-    def initialize(institution,course , start_date,end_date,description)
-        @institution=institution
-        @course=course
-        @start_date=start_date
-        @end_date=end_date
-        @description=description
+    attr_reader :institution,:course,:start_date,:end_date,:description
+    def initialize(**args)
+        @institution=args[:institution]
+        @course=args[:course]
+        @start_date=args[:start_date]
+        @end_date=args[:end_date]
+        @description=args[:description]
     end
-    def gettrainingdetails
-        [@institution,@course,@start_date,@end_date,@description]
-    end
+   
 end
 
 
 class Projects
-    def initialize(name,start_date,end_date,github,link,technology,description)
-        @name=name
-        @start_date=start_date
-        @end_date=end_date
-        @github=github
-        @link=link
-        @technology=technology
-        @description=description
+    attr_reader :name,:start_date,:end_date,:github,:link,:technology,:description
+    def initialize(**args)
+        @name=args[:name]
+        @start_date=args[:start_date]
+        @end_date=args[:end_date]
+        @github=args[:github]
+        @link=args[:link]
+        @technology=args[:technology]
+        @description=args[:description]
     end
-    def getprojectdetails
-        [@name,@start_date,@end_date,@github,@link,@technology,@description]
-    end
+    
 end
 
 
+puts "What is your name?"
+name = gets.chomp
 
+puts "What is your email address?"
+email = gets.chomp
 
-person = PersonalDetails.new(name: "John Smith",email: "john.smith@example.com", mobile: "555-1234",place: "New York",preferred_location: "San Francisco")
+puts "What is your mobile number?"
+mobile = gets.chomp
 
-# person = PersonalDetails.new("John Smith", "john.smith@example.com", "555-1234", "New York", "San Francisco")
+puts "Where do you live?"
+place = gets.chomp
+
+puts "What is your preferred location?"
+preferred_location = gets.chomp
+
+person = PersonalDetails.new(name: name,email: email, mobile: mobile,place: place,preferred_location: preferred_location)
+
  
 
 
-experience = Experience.new("mindfire","software engineer","20-12-2022","20-12-2023")
+experience = Experience.new(company_name:"mindfire",current_post:"software engineer",joining_date:"20-12-2022",exiting_date:"20-12-2023")
  
 
 
-skill = Skill.new("React Js ","intermediate","1year")
- 
-
-
-
-qualifications = Qualifications.new("B.sc","Gujarati","DAVV" , "12-12-2020","12-12-2023","80%")
+skill = Skill.new(name:"React Js ",level:"intermediate",duration:"1year")
  
 
 
 
-training = Training.new("code better","React js" , "12-12-2020","12-12-2023","best institute ever!")
+qualifications = Qualifications.new(course:"B.sc",college:"Gujarati",board:"DAVV" ,start_date:"12-12-2020",end_date:"12-12-2023",marks:"80%")
  
 
 
 
-projects = Projects.new("Resume Builder","1-3-2023","2-3-2023","unknown@github.com","http://rubymonk.com/learning/books","reactjs","make best resume!")
+training = Training.new(institution:"code better",course:"React js" , start_date:"12-12-2020",end_date:"12-12-2023",description:"best institute ever!")
+ 
+
+
+
+projects = Projects.new(name:"Resume Builder",start_date:"1-3-2023",end_date:"2-3-2023",github:"unknown@github.com",link:"http://rubymonk.com/learning/books",technology:"reactjs",description:"make best resume!")
  
 
 
@@ -140,53 +145,53 @@ projects = Projects.new("Resume Builder","1-3-2023","2-3-2023","unknown@github.c
         file.puts("\n")
   
         file.puts("Experience:")
-        file.puts("Company name: #{@experience.getexperiencedetails[0]}")
-        file.puts("Current post: #{@experience.getexperiencedetails[1]}")
-        file.puts("Joining date: #{@experience.getexperiencedetails[2]}")
-        file.puts("Exiting date: #{@experience.getexperiencedetails[3]}")
+        file.puts("Company name: #{@experience.company_name}")
+        file.puts("Current post: #{@experience.current_post}")
+        file.puts("Joining date: #{@experience.joining_date}")
+        file.puts("Exiting date: #{@experience.exiting_date}")
         file.puts("\n")
   
         file.puts("Skills:")
-        file.puts("Name: #{@skill.getskilldetails[0]}")
-        file.puts("Level: #{@skill.getskilldetails[1]}")
-        file.puts("Duration: #{@skill.getskilldetails[2]}")
+        file.puts("Name: #{@skill.name}")
+        file.puts("Level: #{@skill.level}")
+        file.puts("Duration: #{@skill.duration}")
         file.puts("\n")
   
         file.puts("Qualifications:")
-        file.puts("Course: #{@qualifications.getqualificationsdetails[0]}")
-        file.puts("College: #{@qualifications.getqualificationsdetails[1]}")
-        file.puts("Board: #{@qualifications.getqualificationsdetails[2]}")
-        file.puts("Start date: #{@qualifications.getqualificationsdetails[3]}")
-        file.puts("End date: #{@qualifications.getqualificationsdetails[4]}")
-        file.puts("Marks: #{@qualifications.getqualificationsdetails[5]}")
+        file.puts("Course: #{@qualifications.course}")
+        file.puts("College: #{@qualifications.college}")
+        file.puts("Board: #{@qualifications.board}")
+        file.puts("Start date: #{@qualifications.start_date}")
+        file.puts("End date: #{@qualifications.end_date}")
+        file.puts("Marks: #{@qualifications.marks}")
         file.puts("\n")
   
         file.puts("Training:")
-        file.puts("Institution: #{@training.gettrainingdetails[0]}")
-        file.puts("Course: #{@training.gettrainingdetails[1]}")
-        file.puts("Start date: #{@training.gettrainingdetails[2]}")
-        file.puts("End date: #{@training.gettrainingdetails[3]}")
-        file.puts("Description: #{@training.gettrainingdetails[4]}")
+        file.puts("Institution: #{@training.institution}")
+        file.puts("Course: #{@training.course}")
+        file.puts("Start date: #{@training.start_date}")
+        file.puts("End date: #{@training.end_date}")
+        file.puts("Description: #{@training.description}")
         file.puts("\n")
   
         file.puts("Projects:")
-        file.puts("Name: #{@projects.getprojectdetails[0]}")
-        file.puts("Start date: #{@projects.getprojectdetails[1]}")
-        file.puts("End date: #{@projects.getprojectdetails[2]}")
-        file.puts("Github: #{@projects.getprojectdetails[3]}")
-        file.puts("Link: #{@projects.getprojectdetails[4]}")
-        file.puts("Technology: #{@projects.getprojectdetails[5]}")
-        file.puts("Description: #{@projects.getprojectdetails[6]}")
+        file.puts("Name: #{@projects.name}")
+        file.puts("Start date: #{@projects.start_date}")
+        file.puts("End date: #{@projects.end_date}")
+        file.puts("Github: #{@projects.github}")
+        file.puts("Link: #{@projects.link}")
+        file.puts("Technology: #{@projects.technology}")
+        file.puts("Description: #{@projects.description}")
         file.puts("\n")
       end
     end
   end
   
   resume = ResumeBuilder.new(person, experience, skill, qualifications, training, projects)
-  resume.create_resume("resumenew3.txt")
+#   resume.create_resume("resumenew5.txt")
   
 
-  File.open("resumenew2.txt", "r") {|file| puts file.read }
+  File.open("resumenew5.txt", "r") {|file| puts file.read }
   
 
 
